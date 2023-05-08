3d30ec90eca65cf88583a2b5213a9bb1b24b1ecc
paytonburks <payton.burks7@gmail.com>
2023-05-07 19:23:48 -0700

    commit

--- twitter/fetch_tweets_git.py
+++ twitter/fetch_tweets_git.py
@@ -19,14 +19,15 @@ def insert_rows_to_bq(rows):
         print(errors)
 
 def fetch_recent_tweets_for_user(client, user_id, start_time):
-    response = client.get_users_tweets(user_id, tweet_fields=["created_at"], start_time=start_time)
+    response = client.get_users_tweets(user_id, tweet_fields=["created_at", "public_metrics", "source"], start_time=start_time)
     # print(type(response.data[0]))
     rows = []
     if response.data is not None:
+        # print(response.data)
         for tweet in response.data:
             # print(tweet.created_at)
             created_at = pd.Timestamp(tweet.created_at).strftime("%Y-%m-%d %H:%M:%S")
-            values = {"tweet_id": tweet.id, "author_id": user_id, "created_at": created_at, "text": tweet.text}
+            values = {"tweet_id": tweet.id, "created_at": created_at, "text": tweet.text, "replies": tweet.public_metrics['reply_count'], "retweets": tweet.public_metrics['retweet_count'], "likes": tweet.public_metrics['like_count'], "source": tweet.source}
             rows.append(values)
     return rows
 
@@ -39,14 +40,27 @@ def entry_point(request): # request is a Flask request
 
     bearer_token = os.environ.get("BEARER_TOKEN")
     client = tweepy.Client(bearer_token=token)
+    client.get_users_tweets(user_id, tweet_fields=[])
 
     start_time = datetime.now(timezone.utc) - timedelta(hours=24)
     start_time = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
     tweet_rows = fetch_recent_tweets_for_user(client, user_id, start_time)
-
     print(tweet_rows)
 
     if len(tweet_rows) > 0:
-        insert_rows_to_bq(tweet_rows)
+        df = pd.read_csv('tweets/thugtweets.csv')
+        df = df.drop(columns=['Unnamed: 0'])
+
+        for t in tweet_rows:
+            row = []
+            row.extend([t['created_at']+'+00:00', t['likes'], t['replies'], t['retweets'], 'Twitter for iPhone', t['text']])
+            df.loc[len(df.index)] = row
+        
+        df.to_csv('tweets/thugtweets.csv')
+
+    # if len(tweet_rows) > 0:
+    #     insert_rows_to_bq(tweet_rows)
         
     return "Success", 200
+
+entry_point(None)
