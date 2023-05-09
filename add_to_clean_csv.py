14e03f4fc88c63952fd2d54f6c8ad5c2d8641531
paytonburks <payton.burks7@gmail.com>
2023-05-08 22:49:41 -0700

    commit

--- add_to_clean_csv.py
+++ add_to_clean_csv.py
@@ -1,11 +1,19 @@
 from twitter import fetch_tweets_git as ft
-
+from sentiment import analysis, conversation
+import transform_df as trans
 
 def main():
-    ft.entry_point(None)
-    
+    print("ENTERED")
+    num = ft.entry_point(None)
+    if len(num) > 0:
+        analysis.main()
+        trans.thug_update_one()
+        conversation.main()
+        trans.thug_update_two()
+        analysis.update()
+        trans.thug_final_update()
+        print("PASSED")
     return
-
 
 if __name__ == "__main__":
     main()
